0a0689a32922d4b099e15866a3704fffa2f3b4f2
Akshayraut123 <akshayraut26117@gmail.com>
2023-01-03 17:15:29 +0530

    first commit.....

---
+++ github_project_new/src/github_project_new/Test1.java
@@ -0,0 +1,15 @@
+package github_project_new;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	String a="akshay";
+	
+	if (a=="akshay") {
+		System.out.println("true");
+	}
+	}
+
+}
